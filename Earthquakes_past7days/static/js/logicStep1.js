a4281a14a663b3bab7485c42a6e5ab2141cf55bc
RoopaRaghav <111100908+RoopaRaghav@users.noreply.github.com>
2022-12-16 19:09:46 -0800

    Added Earthquake mapping files.

---
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -0,0 +1,74 @@
+// Create the map object with center and zoom level.
+// map = L.map('mapid').setView([30, 30], 2);
+
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoHoods = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+maxZoom: 18,
+accessToken: API_KEY
+});
+
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    
+   accessToken: API_KEY
+});
+
+let baseMaps = {
+  Streets : streets,
+  SatelliteStreets: satelliteStreets
+};
+
+// Create a style for the lines.
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center:[39.5, -98.5],
+    zoom: 3,
+    layers: [satelliteStreets] //default layer
+})
+
+
+//streets.addTo(map);
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+
+function onEachFeature(feature, layer) {
+  // does this feature have a property named popupContent?
+  console.log(feature);
+  
+  if (feature.properties.place && feature.properties.mag) {
+      layer.bindPopup("<h3> "+feature.properties.place + "</h4> </hr> <h4> Magnitude: "+ feature.properties.mag + "</h4>");
+  }
+}
+
+let myStyle = {
+  color: "blue",
+  weight: 1,
+  fillOpacity: 0.7,
+  fillColor: "lightyellow",
+}
+
+// // Grabbing our GeoJSON data.
+d3.json(torontoHoods).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data,{
+  onEachFeature: onEachFeature,
+  // color:"lightyellow",
+  // weight: 2
+  style: myStyle
+
+}).addTo(map);
+});
+
+
